3d6337001fa40790eb78413f410695c3fe1c817d
John Simons <john.simons@nservicebus.com>
2014-03-18 11:18:37 +1000

    NHibernate Saga persister does not work with Oracle
    The issue is that the Oracle driver does not support both TransactionScope and NH transaction, see http://stackoverflow.com/q/6870901/90882 for more info.
    So we now checking if there is currently an active ambient transaction and if there is we do not create a NH transaction.
    We also disable AutoFlush and control Flush manually.

---
+++ src/NServiceBus.NHibernate.AcceptanceTests-Oracle/Config.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Specialized;
+using System.Configuration;
+using NServiceBus;
+using NServiceBus.Persistence.NHibernate;
+
+public class ConfigureSagaPersister
+{
+    public void Configure(Configure config)
+    {
+        NHibernateSettingRetriever.ConnectionStrings = () => new ConnectionStringSettingsCollection
+        {
+            new ConnectionStringSettings("NServiceBus/Persistence", @"Data Source=XE;User Id=john;Password=Welcome1")
+        };
+
+        NHibernateSettingRetriever.AppSettings = () => new NameValueCollection
+                                                               {
+                                                                   {"NServiceBus/Persistence/NHibernate/connection.driver_class", "NHibernate.Driver.OracleDataClientDriver"},
+                                                                   {"NServiceBus/Persistence/NHibernate/dialect", "NHibernate.Dialect.Oracle10gDialect"}
+                                                               };
+
+        config.UseNHibernateSagaPersister();
+    }
+}
