b8f07cb2272ea6f12bc6f553db97cda614d99c48
John Simons <john.simons@nservicebus.com>
2014-03-20 12:00:49 +1000

    Added table naming convention for Oracle tests

--- src/NServiceBus.NHibernate.AcceptanceTests-Oracle/Config.cs
+++ src/NServiceBus.NHibernate.AcceptanceTests-Oracle/Config.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Specialized;
+﻿using System;
+using System.Collections.Specialized;
 using System.Configuration;
+using System.Security.Cryptography;
+using System.Text;
 using NServiceBus;
 using NServiceBus.Persistence.NHibernate;
+using NServiceBus.Saga;
 
 public class ConfigureSagaPersister
 {
@@ -18,6 +22,43 @@ public class ConfigureSagaPersister
                                                                    {"NServiceBus/Persistence/NHibernate/dialect", "NHibernate.Dialect.Oracle10gDialect"}
                                                                };
 
-        config.UseNHibernateSagaPersister();
+        config.UseNHibernateSagaPersister(type =>
+        {
+            var tablename = DefaultTableNameConvention(type);
+
+            if (tablename.Length > 30) //Oracle limit and it has to start with an Alpha character
+            {
+                return Create(tablename);
+            }
+
+            return tablename;
+        });
+    }
+    
+    static string Create(params object[] data)
+    {
+        using (var provider = new MD5CryptoServiceProvider())
+        {
+            var inputBytes = Encoding.Default.GetBytes(String.Concat(data));
+            var hashBytes = provider.ComputeHash(inputBytes);
+            // generate a guid from the hash:
+            return "A" + new Guid(hashBytes).ToString("N").Substring(0, 20);
+        }
+    }
+
+    static string DefaultTableNameConvention(Type type)
+    {
+        //if the type is nested use the name of the parent
+        if (type.DeclaringType == null)
+        {
+            return type.Name;
+        }
+
+        if (typeof(IContainSagaData).IsAssignableFrom(type))
+        {
+            return type.DeclaringType.Name;
+        }
+
+        return type.DeclaringType.Name + "_" + type.Name;
     }
 }
