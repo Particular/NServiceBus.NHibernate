43ec98d6fdf7971ddbedd3ff9f2b339a7b49301f
John Simons <john.simons@nservicebus.com>
2014-05-22 16:02:12 +1000

    Need to shorten saga names

--- src/NServiceBus.NHibernate.AcceptanceTests-Oracle/ConfigureNHibernatePersistence.cs
+++ src/NServiceBus.NHibernate.AcceptanceTests-Oracle/ConfigureNHibernatePersistence.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Specialized;
+﻿using System;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Text;
 using NServiceBus;
 using NServiceBus.NHibernate;
 using NServiceBus.NHibernate.Internal;
 using NServiceBus.Persistence;
+using NServiceBus.Saga;
 
 public class ConfigureNHibernatePersistence
 {
@@ -13,6 +17,47 @@ public class ConfigureNHibernatePersistence
             {"NServiceBus/Persistence/NHibernate/connection.driver_class", "NHibernate.Driver.OracleDataClientDriver"},
             {"NServiceBus/Persistence/NHibernate/dialect", "NHibernate.Dialect.Oracle10gDialect"}
         };
-        config.UsePersistence<NServiceBus.Persistence.NHibernate>(c => c.ConnectionString(@"Data Source=XE;User Id=particular;Password=Welcome1"));
+        config.UsePersistence<NServiceBus.Persistence.NHibernate>(c =>
+        {
+            c.ConnectionString(@"Data Source=XE;User Id=particular;Password=Welcome1");
+            c.SagaTableNamingConvention(type=>
+            {
+                var tablename = DefaultTableNameConvention(type);
+
+                if (tablename.Length > 30) //Oracle limit and it has to start with an Alpha character
+                {
+                    return Create(tablename);
+                }
+
+                return tablename;
+            });
+        });
+    }
+
+    static string Create(params object[] data)
+    {
+        using (var provider = new MD5CryptoServiceProvider())
+        {
+            var inputBytes = Encoding.Default.GetBytes(String.Concat(data));
+            var hashBytes = provider.ComputeHash(inputBytes);
+            // generate a guid from the hash:
+            return "A" + new Guid(hashBytes).ToString("N").Substring(0, 20);
+        }
+    }
+
+    static string DefaultTableNameConvention(Type type)
+    {
+        //if the type is nested use the name of the parent
+        if (type.DeclaringType == null)
+        {
+            return type.Name;
+        }
+
+        if (typeof(IContainSagaData).IsAssignableFrom(type))
+        {
+            return type.DeclaringType.Name;
+        }
+
+        return type.DeclaringType.Name + "_" + type.Name;
     }
 }
