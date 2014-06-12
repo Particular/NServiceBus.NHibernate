aa633e649b887752aa367513a2d59988abf06ede
Andreas Öhlund <andreas.ohlund@particular.net>
2014-06-12 16:26:05 +0200

    Updating with storage capabilities

--- src/NServiceBus.NHibernate/NHibernate.cs
+++ src/NServiceBus.NHibernate/NHibernate.cs
@@ -7,6 +7,16 @@
     /// </summary>
     public class NHibernate : PersistenceDefinition
     {
-
+        /// <summary>
+        /// Constructor that defines the capabilities of the storage
+        /// </summary>
+        public NHibernate()
+        {
+            HasGatewaysStorage = true;
+            HasOutboxStorage = true;
+            HasSagaStorage = true;
+            HasSubscriptionStorage = true;
+            HasTimeoutStorage = true;
+        }
     }
 }
