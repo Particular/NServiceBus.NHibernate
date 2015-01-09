d7c16a20a3bf6796dd7b9105c9d2996c605b0da5
John Simons <john.simons@nservicebus.com>
2015-01-09 12:20:06 +1000

    Removed default value for Dispatched column
    PostgreSql does not support setting bit column to default = 0
    
    Fixed #74

--- src/NServiceBus.NHibernate/Outbox/EntityMappings.cs
+++ src/NServiceBus.NHibernate/Outbox/EntityMappings.cs
@@ -20,11 +20,7 @@ namespace NServiceBus.Outbox.NHibernate
             });
             Property(p => p.Dispatched, pm =>
             {
-                pm.Column(c =>
-                {
-                    c.Default(0);
-                    c.NotNullable(true);
-                });
+                pm.Column(c => c.NotNullable(true));
                 pm.Index("OutboxRecord_Dispatched_Index");
             });
             Property(p => p.DispatchedAt, pm => pm.Index("OutboxRecord_DispatchedAt_Index"));
