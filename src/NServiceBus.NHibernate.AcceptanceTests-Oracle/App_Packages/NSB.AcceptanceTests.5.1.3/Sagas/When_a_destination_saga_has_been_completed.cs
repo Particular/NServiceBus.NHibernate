07ce20d02c7a86c0c7895eb5f73b77c069fe5684
SzymonPobiega <szymon.pobiega@gmail.com>
2015-04-22 05:41:48 +0200

    Split up a test to avoid sharing state

--- src/NServiceBus.NHibernate.AcceptanceTests-Oracle/App_Packages/NSB.AcceptanceTests.5.1.3/Sagas/When_a_destination_saga_has_been_completed.cs
+++ src/NServiceBus.NHibernate.AcceptanceTests-Oracle/App_Packages/NSB.AcceptanceTests.5.1.3/Sagas/When_a_destination_saga_has_been_completed.cs
@@ -7,36 +7,42 @@
     using Saga;
     using ScenarioDescriptors;
 
-    public class When_receiving_that_completes_the_saga : NServiceBusAcceptanceTest
-    {
+    public class When_a_destination_saga_has_been_completed
+    { 
         [Test]
-        public void Should_hydrate_and_complete_the_existing_instance()
+        public void Should_ignore_messages_afterwards()
         {
-            Scenario.Define(() => new Context
+            var context = new Context
             {
                 Id = Guid.NewGuid()
-            })
+            };
+
+            Scenario.Define(context)
                 .WithEndpoint<SagaEndpoint>(b =>
                 {
-                    b.Given((bus, context) => bus.SendLocal(new StartSagaMessage
+                    b.Given((bus, c) => bus.SendLocal(new StartSagaMessage
                     {
-                        SomeId = context.Id
+                        SomeId = c.Id
                     }));
-                    b.When(context => context.StartSagaMessageReceived, (bus, context) =>
+                    b.When(c => c.StartSagaMessageReceived, (bus, c) =>
                     {
-                        context.AddTrace("CompleteSagaMessage sent");
-
+                        c.AddTrace("CompleteSagaMessage sent");
                         bus.SendLocal(new CompleteSagaMessage
                         {
-                            SomeId = context.Id
+                            SomeId = c.Id
                         });
                     });
+                    b.When(c => c.SagaCompleted, (bus, c) => bus.SendLocal(new AnotherMessage
+                    {
+                        SomeId = c.Id
+                    }));
                 })
-                .Done(c => c.SagaCompleted)
+                .Done(c => c.AnotherMessageReceived)
                 .Repeat(r => r.For(Transports.Default))
-                .Should(c => Assert.True(c.SagaCompleted))
-
                 .Run();
+
+            Assert.True(context.AnotherMessageReceived, "AnotherMessage should have been delivered to the handler outside the saga");
+            Assert.False(context.SagaReceivedAnotherMessage, "AnotherMessage should not be delivered to the saga after completion");
         }
 
         public class Context : ScenarioContext
@@ -55,9 +61,10 @@
                 EndpointSetup<DefaultServer>(b => b.LoadMessageHandlers<First<TestSaga>>());
             }
 
-            public class TestSaga : Saga<TestSagaData>,
-                IAmStartedByMessages<StartSagaMessage>,
-                IHandleMessages<CompleteSagaMessage>,
+            public class TestSaga : Saga<TestSagaData>, 
+                IAmStartedByMessages<StartSagaMessage>, 
+                IHandleMessages<CompleteSagaMessage>, 
+                IHandleMessages<AnotherMessage>,
                 IHandleSagaNotFound
             {
                 public Context Context { get; set; }
@@ -78,16 +85,29 @@
                     Context.SagaCompleted = true;
                 }
 
+                public void Handle(AnotherMessage message)
+                {
+                    Context.AddTrace("AnotherMessage received");
+                    Context.SagaReceivedAnotherMessage = true;
+                }
+
                 protected override void ConfigureHowToFindSaga(SagaPropertyMapper<TestSagaData> mapper)
                 {
                     mapper.ConfigureMapping<StartSagaMessage>(m => m.SomeId)
                         .ToSaga(s => s.SomeId);
                     mapper.ConfigureMapping<CompleteSagaMessage>(m => m.SomeId)
                         .ToSaga(s => s.SomeId);
+                    mapper.ConfigureMapping<AnotherMessage>(m => m.SomeId)
+                        .ToSaga(s => s.SomeId);
                 }
 
                 public void Handle(object message)
                 {
+                    if (message is AnotherMessage)
+                    {
+                        return;
+                    }
+
                     throw new Exception("Unexpected 'saga not found' for message: " + message.GetType().Name);
                 }
             }
@@ -102,6 +122,15 @@
             }
         }
 
+        public class CompletionHandler : IHandleMessages<AnotherMessage>
+        {
+            public Context Context { get; set; }
+            public void Handle(AnotherMessage message)
+            {
+                Context.AnotherMessageReceived = true;
+            }
+        }
+
         [Serializable]
         public class StartSagaMessage : ICommand
         {
@@ -110,6 +139,12 @@
 
         [Serializable]
         public class CompleteSagaMessage : ICommand
+        {
+            public Guid SomeId { get; set; }
+        }
+
+        [Serializable]
+        public class AnotherMessage : ICommand
         {
             public Guid SomeId { get; set; }
         }
