168002c58c7058189e247713fb981b12f939aa2f
John Simons <john.simons@nservicebus.com>
2015-04-28 12:04:53 +1000

    Throwing an exception is schema update fails
    Fixes #93

--- src/NServiceBus.NHibernate/SharedSession/Installer.cs
+++ src/NServiceBus.NHibernate/SharedSession/Installer.cs
@@ -1,5 +1,8 @@
 namespace NServiceBus.Persistence.NHibernate
 {
+    using System;
+    using System.Linq;
+    using System.Text;
     using global::NHibernate.Cfg;
     using global::NHibernate.Tool.hbm2ddl;
     using Installation;
@@ -14,7 +17,24 @@ namespace NServiceBus.Persistence.NHibernate
         {
             if (RunInstaller)
             {
-                new SchemaUpdate(configuration).Execute(false, true);
+                var schemaUpdate = new SchemaUpdate(configuration);
+                var sb = new StringBuilder();
+                schemaUpdate.Execute(s => sb.AppendLine(s), true);
+
+                if (!schemaUpdate.Exceptions.Any())
+                {
+                    return;
+                }
+
+                var aggregate = new AggregateException(schemaUpdate.Exceptions);
+
+                var errorMessage = @"Schema update failed.
+The following exception(s) were thrown:
+{0}
+
+TSql Script:
+{1}";
+                throw new Exception(String.Format(errorMessage, aggregate, sb));
             }
         }
     }
