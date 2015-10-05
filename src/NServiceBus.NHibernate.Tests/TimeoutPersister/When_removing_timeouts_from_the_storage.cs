b4f38e292e210892e8e8938dd507ca1f7507a44f
Tim Bussmann <tim.bussmann@particular.net>
2015-10-05 17:39:56 +0200

    implement IPersistTimeoutsV2

--- src/NServiceBus.NHibernate.Tests/TimeoutPersister/When_removing_timeouts_from_the_storage.cs
+++ src/NServiceBus.NHibernate.Tests/TimeoutPersister/When_removing_timeouts_from_the_storage.cs
@@ -2,6 +2,9 @@ namespace NServiceBus.TimeoutPersisters.NHibernate.Tests
 {
     using System;
     using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Transactions;
     using NUnit.Framework;
     using Support;
     using Timeout.Core;
@@ -10,7 +13,7 @@ namespace NServiceBus.TimeoutPersisters.NHibernate.Tests
     class When_removing_timeouts_from_the_storage : InMemoryDBFixture
     {
         [Test]
-        public void Should_return_the_correct_headers()
+        public void TryRemove_should_return_the_correct_headers()
         {
             var headers = new Dictionary<string, string>
                           {
@@ -20,14 +23,14 @@ namespace NServiceBus.TimeoutPersisters.NHibernate.Tests
                           };
 
             var timeout = new TimeoutData
-                          {
-                              Time = DateTime.UtcNow.AddHours(-1),
-                              Destination = new Address("timeouts", RuntimeEnvironment.MachineName),
-                              SagaId = Guid.NewGuid(),
-                              State = new byte[] {1, 1, 133, 200},
-                              Headers = headers,
-                              OwningTimeoutManager = "MyTestEndpoint",
-                          };
+            {
+                Time = DateTime.UtcNow.AddHours(-1),
+                Destination = new Address("timeouts", RuntimeEnvironment.MachineName),
+                SagaId = Guid.NewGuid(),
+                State = new byte[] { 1, 1, 133, 200 },
+                Headers = headers,
+                OwningTimeoutManager = "MyTestEndpoint",
+            };
             persister.Add(timeout);
 
             TimeoutData timeoutData;
@@ -37,26 +40,26 @@ namespace NServiceBus.TimeoutPersisters.NHibernate.Tests
         }
 
         [Test]
-        public void Should_remove_timeouts_by_id()
+        public void TryRemove_should_remove_timeouts_by_id()
         {
             var t1 = new TimeoutData
-                     {
-                         Time = DateTime.Now.AddYears(-1),
-                         OwningTimeoutManager = "MyTestEndpoint",
-                         Headers = new Dictionary<string, string>
+            {
+                Time = DateTime.Now.AddYears(-1),
+                OwningTimeoutManager = "MyTestEndpoint",
+                Headers = new Dictionary<string, string>
                                    {
                                        {"Header1", "Value1"}
                                    }
-                     };
+            };
             var t2 = new TimeoutData
-                     {
-                         Time = DateTime.Now.AddYears(-1),
-                         OwningTimeoutManager = "MyTestEndpoint",
-                         Headers = new Dictionary<string, string>
+            {
+                Time = DateTime.Now.AddYears(-1),
+                OwningTimeoutManager = "MyTestEndpoint",
+                Headers = new Dictionary<string, string>
                                    {
                                        {"Header1", "Value1"}
                                    }
-                     };
+            };
 
             persister.Add(t1);
             persister.Add(t2);
@@ -78,34 +81,95 @@ namespace NServiceBus.TimeoutPersisters.NHibernate.Tests
         }
 
         [Test]
-        public void Should_remove_timeouts_by_sagaid()
+        public void TryRemove_should_return_false_when_timeout_already_deleted()
+        {
+            var timeout = new TimeoutData();
+
+            persister.Add(timeout);
+
+            Assert.IsTrue(persister.TryRemove(timeout.Id));
+            Assert.IsFalse(persister.TryRemove(timeout.Id));
+        }
+
+        [Test]
+        public void TryRemove_should_work_with_concurrent_transactions()
+        {
+            var timeout = new TimeoutData
+            {
+                Time = DateTime.Now
+            };
+
+            persister.Add(timeout);
+
+            var t1EnteredTx = new AutoResetEvent(false);
+            var t2EnteredTx = new AutoResetEvent(false);
+
+            var task1 = Task.Factory.StartNew(() =>
+            {
+                using (var tx = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted
+                }))
+                {
+                    t1EnteredTx.Set();
+                    t2EnteredTx.WaitOne();
+                    var result = persister.TryRemove(timeout.Id);
+                    tx.Complete();
+                    return result;
+                }
+            });
+
+            var task2 = Task.Factory.StartNew(() =>
+            {
+                using (var tx = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted
+                }))
+                {
+                    t2EnteredTx.Set();
+                    t1EnteredTx.WaitOne();
+                    var result = persister.TryRemove(timeout.Id);
+                    tx.Complete();
+                    return result;
+                }
+            });
+
+            Assert.True(task1.Wait(TimeSpan.FromSeconds(30)));
+            Assert.True(task2.Wait(TimeSpan.FromSeconds(30)));
+
+            // one delete should succeed, the other one shouldn't
+            Assert.IsTrue(task1.Result || task2.Result);
+            Assert.IsFalse(task1.Result && task2.Result);
+        }
+
+        [Test]
+        public void RemoveTimeoutBy_should_remove_timeouts_by_sagaid()
         {
             var sagaId1 = Guid.NewGuid();
             var sagaId2 = Guid.NewGuid();
             var t1 = new TimeoutData
-                     {
-                         SagaId = sagaId1,
-                         Time = DateTime.Now.AddYears(1),
-                         OwningTimeoutManager = "MyTestEndpoint",
-                         Headers = new Dictionary<string, string>
+            {
+                SagaId = sagaId1,
+                Time = DateTime.Now.AddYears(1),
+                OwningTimeoutManager = "MyTestEndpoint",
+                Headers = new Dictionary<string, string>
                                    {
                                        {"Header1", "Value1"}
                                    }
-                     };
+            };
             var t2 = new TimeoutData
-                     {
-                         SagaId = sagaId2,
-                         Time = DateTime.Now.AddYears(1),
-                         OwningTimeoutManager = "MyTestEndpoint",
-                         Headers = new Dictionary<string, string>
+            {
+                SagaId = sagaId2,
+                Time = DateTime.Now.AddYears(1),
+                OwningTimeoutManager = "MyTestEndpoint",
+                Headers = new Dictionary<string, string>
                                    {
                                        {"Header1", "Value1"}
                                    }
-                     };
+            };
 
             persister.Add(t1);
             persister.Add(t2);
-
 
             persister.RemoveTimeoutBy(sagaId1);
             persister.RemoveTimeoutBy(sagaId2);
