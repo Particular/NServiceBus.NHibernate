a3f777d9b95cc56c4613328e0cba6893257b49a4
SzymonPobiega <szymon.pobiega@gmail.com>
2016-01-28 11:14:11 +0100

    Fixed it by changing the test so that the saga is actually in the database.

--- src/NServiceBus.NHibernate.AcceptanceTests-Oracle/App_Packages/NSB.AcceptanceTests.6.0.0-unstable1423/Sagas/When_a_finder_exists_and_found_saga.cs
+++ src/NServiceBus.NHibernate.AcceptanceTests-Oracle/App_Packages/NSB.AcceptanceTests.6.0.0-unstable1423/Sagas/When_a_finder_exists_and_found_saga.cs
@@ -44,10 +44,12 @@
                 public Task<TestSaga08.SagaData08> FindBy(SomeOtherMessage message, SynchronizedStorageSession storageSession, ReadOnlyContextBag context)
                 {
                     Context.FinderUsed = true;
-                    return Task.FromResult(new TestSaga08.SagaData08
+                    var sagaData08 = new TestSaga08.SagaData08
                     {
                         Property = "jfbsjdfbsdjh"
-                    });
+                    };
+                    storageSession.Session().Save(sagaData08);
+                    return Task.FromResult(sagaData08);
                 }
             }
 
