1c3561ac5e928bb23e7d4353e741957f49e2404a
Hadi Eskandari <h.eskandari@gmail.com>
2016-02-09 17:09:30 +1100

    Using Newtonsoft Json serializer and ILMerging it in release

--- src/NServiceBus.NHibernate/Internal/ObjectSerializer.cs
+++ src/NServiceBus.NHibernate/Internal/ObjectSerializer.cs
@@ -1,38 +1,17 @@
 ﻿namespace NServiceBus.Persistence.NHibernate
 {
-    using System;
-    using System.IO;
-    using System.Runtime.Serialization.Json;
-    using System.Text;
+    using Newtonsoft.Json;
 
     static class ObjectSerializer
     {
         public static string Serialize<T>(T instance)
         {
-            var serializer = BuildSerializer(typeof(T));
-            using (var stream = new MemoryStream())
-            {
-                serializer.WriteObject(stream, instance);
-                return Encoding.UTF8.GetString(stream.ToArray());
-            }
+            return JsonConvert.SerializeObject(instance);
         }
 
         public static T DeSerialize<T>(string data)
         {
-            var serializer = BuildSerializer(typeof(T));
-            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(data)))
-            {
-                return (T)serializer.ReadObject(stream);
-            }
-        }
-
-        static DataContractJsonSerializer BuildSerializer(Type objectType)
-        {
-            var settings = new DataContractJsonSerializerSettings
-            {
-                UseSimpleDictionaryFormat = true
-            };
-            return new DataContractJsonSerializer(objectType, settings);
+            return JsonConvert.DeserializeObject<T>(data);
         }
     }
 }
