2921ced134ed409c2763dd350333c765e456b6f0
Hadi Eskandari <h.eskandari@gmail.com>
2016-02-10 11:26:23 +1100

    Adding serialization and backward compat tests

--- src/NServiceBus.NHibernate/Internal/ObjectSerializer.cs
+++ src/NServiceBus.NHibernate/Internal/ObjectSerializer.cs
@@ -1,39 +1,39 @@
 ﻿namespace NServiceBus.Persistence.NHibernate
 {
-	using System;
-	using System.IO;
-	using System.Runtime.Serialization.Json;
-	using System.Text;
+    using System;
+    using System.IO;
+    using System.Runtime.Serialization.Json;
+    using System.Text;
 
-	static class ObjectSerializer
-	{
-		public static string Serialize<T>(T instance)
-		{
-			var serializer = BuildSerializer(typeof(T));
-			using (var stream = new MemoryStream())
-			{
-				serializer.WriteObject(stream, instance);
-				return Encoding.UTF8.GetString(stream.ToArray());
-			}
-		}
+    static class ObjectSerializer
+    {
+        public static string Serialize<T>(T instance)
+        {
+            var serializer = BuildSerializer(typeof(T));
+            using (var stream = new MemoryStream())
+            {
+                serializer.WriteObject(stream, instance);
+                return Encoding.UTF8.GetString(stream.ToArray());
+            }
+        }
 
-		public static T DeSerialize<T>(string data)
-		{
-			var serializer = BuildSerializer(typeof(T));
-			using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(data)))
-			{
-				return (T)serializer.ReadObject(stream);
-			}
-		}
+        public static T DeSerialize<T>(string data)
+        {
+            var serializer = BuildSerializer(typeof(T));
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(data)))
+            {
+                return (T)serializer.ReadObject(stream);
+            }
+        }
 
-		static DataContractJsonSerializer BuildSerializer(Type objectType)
-		{
-			var settings = new DataContractJsonSerializerSettings
-			{
-				UseSimpleDictionaryFormat = true,
+        static DataContractJsonSerializer BuildSerializer(Type objectType)
+        {
+            var settings = new DataContractJsonSerializerSettings
+            {
+                UseSimpleDictionaryFormat = true,
 
-			};
-			return new DataContractJsonSerializer(objectType, settings);
-		}
-	}
+            };
+            return new DataContractJsonSerializer(objectType, settings);
+        }
+    }
 }
