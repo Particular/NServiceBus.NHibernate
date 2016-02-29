8fe647a248d3842fa678545c6a66a58c0e05e3cf
Daniel Marbach <daniel.marbach@particular.net>
2016-02-29 11:52:30 +0100

    Changed ATTs infrastructure to newest ConfigureEndpointTestExecution

---
+++ src/NServiceBus.NHibernate.AcceptanceTests/ConfigureEndpointNHibernatePersistence.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.AcceptanceTesting.Support;
+using NServiceBus.Persistence;
+
+public class ConfigureScenariosForNHibernatePersistence : IConfigureSupportedScenariosForTestExecution
+{
+    public IEnumerable<Type> UnsupportedScenarioDescriptorTypes { get; } = new List<Type>();
+}
+
+public class ConfigureEndpointNHibernatePersistence : IConfigureEndpointTestExecution
+{
+    public static string ConnectionString
+    {
+        get
+        {
+            var envVar = Environment.GetEnvironmentVariable("NH_ACC_TEST_CONNSTR");
+            if (!string.IsNullOrEmpty(envVar))
+            {
+                return envVar;
+            }
+
+            return defaultConnStr;
+        }
+    }
+
+    public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings)
+    {
+        configuration.UsePersistence<NHibernatePersistence>()
+            .ConnectionString(ConnectionString);
+
+        return Task.FromResult(0);
+    }
+
+    public Task Cleanup()
+    {
+        return Task.FromResult(0);
+    }
+
+    const string defaultConnStr = @"Server=localhost\sqlexpress;Database=nservicebus;Trusted_Connection=True;";
+}
