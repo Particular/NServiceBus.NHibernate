b1b1b63456a0079a931deb541813dfcae66e862c
Hadi Eskandari <h.eskandari@gmail.com>
2016-06-09 16:21:20 +1000

    Adding scenario configuration for sql transport

--- src/NServiceBus.NHibernate.AcceptanceTests/ConfigureEndpointNHibernatePersistence.cs
+++ src/NServiceBus.NHibernate.AcceptanceTests/ConfigureEndpointNHibernatePersistence.cs
@@ -10,45 +10,6 @@ public class ConfigureScenariosForNHibernatePersistence : IConfigureSupportedSce
     public IEnumerable<Type> UnsupportedScenarioDescriptorTypes { get; } = new List<Type>();
 }
 
-public abstract class EndpointConfigurer : IConfigureEndpointTestExecution
-{
-    const string defaultConnStr = @"Server=localhost\SqlExpress;Database=nservicebus;Trusted_Connection=True;";
-
-    public static string ConnectionString
-    {
-        get
-        {
-            var envVar = Environment.GetEnvironmentVariable("NH_ACC_TEST_CONNSTR");
-            if (!string.IsNullOrEmpty(envVar))
-            {
-                return envVar;
-            }
-
-            return defaultConnStr;
-        }
-    }
-
-    public abstract Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings);
-
-    public Task Cleanup()
-    {
-        return Task.FromResult(0);
-    }
-}
-
-public class ConfigureEndpointSqlServerTransport : EndpointConfigurer
-{
-    public override Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings)
-    {
-        configuration.UseTransport<SqlServerTransport>()
-            .ConnectionString(ConnectionString);
-        configuration.UsePersistence<NHibernatePersistence>()
-            .ConnectionString(ConnectionString);
-
-        return Task.FromResult(0);
-    }
-}
-
 public class ConfigureEndpointNHibernatePersistence : EndpointConfigurer
 {
     public override Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings)
