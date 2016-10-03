fce0055a7a266f47bc8247342c6bdc1e727baf4c
Szymon Pobiega <szymon.pobiega@gmail.com>
2016-10-03 10:08:52 +0200

    Fix subscriber version bug (#227)
    * Upgraded to RC3
    
    * Removed old ATs
    
    * Added missing unsubscribe hook.
    
    * Added tests to ensure subscription handling ignores versions.
    
    * Fuxed the laky test problem

--- src/NServiceBus.NHibernate.Tests/SubscriptionStorage/When_using_semantic_versioning_of_messages.cs
+++ src/NServiceBus.NHibernate.Tests/SubscriptionStorage/When_using_semantic_versioning_of_messages.cs
@@ -16,9 +16,33 @@ namespace NServiceBus.Unicast.Subscriptions.NHibernate.Tests
             await storage.Subscribe(TestClients.ClientB, MessageTypes.MessageAv11, new ContextBag());
             await storage.Subscribe(TestClients.ClientC, MessageTypes.MessageAv2, new ContextBag());
 
-            Assert.AreEqual(2, (await storage.GetSubscriberAddressesForMessage(new List<MessageType> { MessageTypes.MessageA }, new ContextBag())).Count()); 
-            Assert.AreEqual(2, (await storage.GetSubscriberAddressesForMessage(new List<MessageType> { MessageTypes.MessageAv11 }, new ContextBag())).Count());
-            Assert.AreEqual(1, (await storage.GetSubscriberAddressesForMessage(new List<MessageType> { MessageTypes.MessageAv2 }, new ContextBag())).Count());
+            Assert.AreEqual(3, (await storage.GetSubscriberAddressesForMessage(new List<MessageType> { MessageTypes.MessageA }, new ContextBag())).Count()); 
+            Assert.AreEqual(3, (await storage.GetSubscriberAddressesForMessage(new List<MessageType> { MessageTypes.MessageAv11 }, new ContextBag())).Count());
+            Assert.AreEqual(3, (await storage.GetSubscriberAddressesForMessage(new List<MessageType> { MessageTypes.MessageAv2 }, new ContextBag())).Count());
+        }
+
+        [Test]
+        public async Task Unsubscribe_should_work_even_if_subscribed_version_is_different()
+        {
+            await storage.Subscribe(TestClients.ClientA, MessageTypes.MessageA, new ContextBag());
+
+            Assert.AreEqual(1, (await storage.GetSubscriberAddressesForMessage(new List<MessageType> { MessageTypes.MessageA }, new ContextBag())).Count());
+        
+            await storage.Unsubscribe(TestClients.ClientA, MessageTypes.MessageAv2, new ContextBag());
+
+            Assert.AreEqual(0, (await storage.GetSubscriberAddressesForMessage(new List<MessageType> { MessageTypes.MessageA }, new ContextBag())).Count());
+        }
+
+        [Test]
+        public async Task Subsribe_should_override_version()
+        {
+            await storage.Subscribe(TestClients.ClientA, MessageTypes.MessageA, new ContextBag());
+
+            Assert.AreEqual(1, (await storage.GetSubscriberAddressesForMessage(new List<MessageType> { MessageTypes.MessageA }, new ContextBag())).Count());
+
+            await storage.Subscribe(TestClients.ClientA, MessageTypes.MessageAv2, new ContextBag());
+
+            Assert.AreEqual(1, (await storage.GetSubscriberAddressesForMessage(new List<MessageType> { MessageTypes.MessageA }, new ContextBag())).Count());
         }
     }
 }
