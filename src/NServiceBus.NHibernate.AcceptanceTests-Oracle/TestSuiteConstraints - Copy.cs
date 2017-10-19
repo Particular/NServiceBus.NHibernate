be26cfa54c0c8128ab6f9982b9e686cffa645dda
Simon Cropp <simon.cropp@gmail.com>
2017-10-19 21:22:00 +1100

    hack around SerializationException

---
+++ src/NServiceBus.NHibernate.AcceptanceTests-Oracle/TestSuiteConstraints - Copy.cs
@@ -0,0 +1,12 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    public partial class NServiceBusAcceptanceTest
+    {
+        static NServiceBusAcceptanceTest()
+        {
+            // Hack: prevents SerializationException ... Type 'x' in assembly 'y' is not marked as serializable.
+            // https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/mitigation-deserialization-of-objects-across-app-domains
+            System.Configuration.ConfigurationManager.GetSection("X");
+        }
+    }
+}
