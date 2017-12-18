4b68bbca7c64748e8d96389393a8b02b8cf4c4dc
andreasohlund <andreas.ohlund@particular.net>
2017-12-18 20:37:58 +0100

    Add gateway att suit

---
+++ src/NServiceBus.NHibernate.AcceptanceTests/GatewayTestSuiteConstraints.cs
@@ -0,0 +1,10 @@
+﻿namespace NServiceBus.Gateway.AcceptanceTests
+{
+    public partial class GatewayTestSuiteConstraints : IGatewayTestSuiteConstraints
+    {
+        public IConfigureGatewayPersitenceExecution CreatePersistenceConfiguration()
+        {
+            return new ConfigureNHibernateGatewayPersistenceExecution();
+        }
+    }
+}
