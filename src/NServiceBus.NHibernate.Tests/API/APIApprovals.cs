12ad557ccc2c0678d3c47bb14b926ce5fb1d1b1c
Brandon Ording <bording@gmail.com>
2018-01-31 18:09:02 -0500

    Add API Approvals test

---
+++ src/NServiceBus.NHibernate.Tests/API/APIApprovals.cs
@@ -0,0 +1,18 @@
+﻿namespace NServiceBus.NHibernate.Tests.API
+{
+    using System.Runtime.CompilerServices;
+    using NUnit.Framework;
+    using PublicApiGenerator;
+
+    [TestFixture]
+    public class APIApprovals
+    {
+        [Test]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Approve()
+        {
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(NHibernatePersistence).Assembly);
+            TestApprover.Verify(publicApi);
+        }
+    }
+}
