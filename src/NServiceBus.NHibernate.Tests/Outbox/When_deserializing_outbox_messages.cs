59335834e746d1fe83fb385d1026dd5712be4590
Szymon Kulec <scooletz@gmail.com>
2018-06-18 13:33:57 +0200

    Inspection fixup

--- src/NServiceBus.NHibernate.Tests/Outbox/When_deserializing_outbox_messages.cs
+++ src/NServiceBus.NHibernate.Tests/Outbox/When_deserializing_outbox_messages.cs
@@ -2,7 +2,6 @@
 {
     using System.Collections.Generic;
     using System.Globalization;
-    using System.Runtime.Serialization.Formatters;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
     using Persistence.NHibernate;
