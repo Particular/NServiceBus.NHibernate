f5876e33d7702c8f393545d021c4ef4fa518b353
Brandon Ording <bording@gmail.com>
2018-06-20 18:48:59 -0400

    Simplify API approval test

--- src/NServiceBus.NHibernate.Tests/API/APIApprovals.cs
+++ src/NServiceBus.NHibernate.Tests/API/APIApprovals.cs
@@ -1,7 +1,5 @@
 ﻿namespace NServiceBus.NHibernate.Tests.API
 {
-    using System;
-    using System.Linq;
     using System.Runtime.CompilerServices;
     using NUnit.Framework;
     using PublicApiGenerator;
@@ -13,19 +11,8 @@
         [MethodImpl(MethodImplOptions.NoInlining)]
         public void Approve()
         {
-            var publicApi = Filter(ApiGenerator.GeneratePublicApi(typeof(NHibernatePersistence).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" }));
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(NHibernatePersistence).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
             TestApprover.Verify(publicApi);
-        }
-
-        static string Filter(string text)
-        {
-            return string.Join(Environment.NewLine, text.Split(new[]
-                {
-                    Environment.NewLine
-                }, StringSplitOptions.RemoveEmptyEntries)
-                .Where(l => !l.StartsWith("[assembly: ReleaseDateAttribute("))
-                .Where(l => !string.IsNullOrWhiteSpace(l))
-            );
         }
     }
 }
