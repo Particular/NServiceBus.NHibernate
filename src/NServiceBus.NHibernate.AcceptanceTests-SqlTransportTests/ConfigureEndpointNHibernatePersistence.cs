67aab96ec76cc8a8387d246325a5f21c44631af1
Szymon Kulec <scooletz@gmail.com>
2018-07-04 12:05:54 +0200

    Tables and indexes made unique for SQL Transport tests

--- src/NServiceBus.NHibernate.AcceptanceTests-SqlTransportTests/ConfigureEndpointNHibernatePersistence.cs
+++ src/NServiceBus.NHibernate.AcceptanceTests-SqlTransportTests/ConfigureEndpointNHibernatePersistence.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NHibernate.Cfg;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Dialect;
 using NServiceBus;
 using NServiceBus.AcceptanceTesting.Support;
 using NServiceBus.Persistence;
@@ -7,9 +11,37 @@ public class ConfigureEndpointNHibernatePersistence : EndpointConfigurer
 {
     public override Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
     {
-        configuration.UsePersistence<NHibernatePersistence>()
-            .ConnectionString(ConnectionString);
+        var cfg = new Configuration
+        {
+            Properties =
+            {
+                {Environment.ConnectionString, ConnectionString},
+                {Environment.Dialect, typeof(MsSql2008Dialect).FullName},
+            },
+        };
 
-        return Task.FromResult(0);
-    }    
+        cfg.BeforeBindMapping += (sender, e) => PrefixMapping(e, "sqlTest_");
+
+        configuration.UsePersistence<NHibernatePersistence>()
+            .UseConfiguration(cfg);
+
+        return Task.CompletedTask;
+    }
+
+    static void PrefixMapping(BindMappingEventArgs e, string prefix)
+    {
+        var c = e.Mapping.RootClasses.Single();
+
+        c.table = prefix + (c.table ?? c.Name);
+        foreach (var prop in c.Properties.OfType<HbmProperty>())
+        {
+            foreach (var column in prop.Columns)
+            {
+                if (!string.IsNullOrEmpty(column.index))
+                {
+                    column.index = prefix + column.index;
+                }
+            }
+        }
+    }
 }
