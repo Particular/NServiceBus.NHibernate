dbeb4f6066e1a76b786701b75695a66f8685cf8b
Brandon Ording <bording@gmail.com>
2018-12-19 17:12:21 -0500

    Switch to Particular.Approvals

--- src/NServiceBus.NHibernate.Tests/API/APIApprovals.cs
+++ src/NServiceBus.NHibernate.Tests/API/APIApprovals.cs
@@ -1,18 +1,17 @@
 ﻿namespace NServiceBus.NHibernate.Tests.API
 {
-    using System.Runtime.CompilerServices;
     using NUnit.Framework;
+    using Particular.Approvals;
     using PublicApiGenerator;
 
     [TestFixture]
     public class APIApprovals
     {
         [Test]
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public void Approve()
         {
             var publicApi = ApiGenerator.GeneratePublicApi(typeof(NHibernatePersistence).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
-            TestApprover.Verify(publicApi);
+            Approver.Verify(publicApi);
         }
     }
 }
