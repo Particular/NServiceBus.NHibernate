dcc3705286207284ee520a2d875398908cef4f9f
danielmarbach <daniel.marbach@openplace.net>
2020-02-12 16:58:25 +0100

    Adjust to API changes

--- src/NServiceBus.NHibernate.AcceptanceTests/GatewayTestSuiteConstraints.cs
+++ src/NServiceBus.NHibernate.AcceptanceTests/GatewayTestSuiteConstraints.cs
@@ -1,10 +1,22 @@
 ﻿namespace NServiceBus.Gateway.AcceptanceTests
 {
+    using System.Threading.Tasks;
+    using AcceptanceTesting.Support;
+    using Persistence;
+
     public partial class GatewayTestSuiteConstraints
     {
-        public IConfigureGatewayPersitenceExecution CreatePersistenceConfiguration()
+        public Task ConfigureDeduplicationStorage(string endpointName, EndpointConfiguration configuration, RunSettings settings)
         {
-            return new ConfigureNHibernateGatewayPersistenceExecution();
+            configuration.UsePersistence<NHibernatePersistence, StorageType.GatewayDeduplication>()
+                .ConnectionString(EndpointConfigurer.ConnectionString);
+
+            return Task.FromResult(0);
+        }
+
+        public Task Cleanup()
+        {
+            return Task.FromResult(0);
         }
     }
 }
