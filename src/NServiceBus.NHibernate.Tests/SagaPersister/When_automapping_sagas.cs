666deadbadc16639352709eed25d4c05ccde5304
Andreas Öhlund <andreas.ohlund@particular.net>
2020-05-12 20:14:13 +0200

    Avoid using settings keys in test

--- src/NServiceBus.NHibernate.Tests/SagaPersister/When_automapping_sagas.cs
+++ src/NServiceBus.NHibernate.Tests/SagaPersister/When_automapping_sagas.cs
@@ -11,8 +11,8 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
     using global::NHibernate.Dialect;
     using NServiceBus.NHibernate.Tests;
     using Sagas;
-    using Settings;
     using NUnit.Framework;
+    using SagaPersisters.NHibernate.AutoPersistence;
 
     [TestFixture]
     public class When_autoMapping_sagas
@@ -23,8 +23,6 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
         [OneTimeSetUp]
         public void SetUp()
         {
-            var builder = new NHibernateSagaStorage();
-
             var cfg = new Configuration()
                 .DataBaseIntegration(x =>
                 {
@@ -32,10 +30,8 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
                     x.ConnectionString = Consts.SqlConnectionString;
                 });
 
-            var settings = new SettingsHolder();
-
             var metaModel = new SagaMetadataCollection();
-            var types = new [] {typeof(TestSaga), typeof(TestSagaData), typeof(TestComponent), typeof(PolymorphicPropertyBase),
+            var types = new[] {typeof(TestSaga), typeof(TestSagaData), typeof(TestComponent), typeof(PolymorphicPropertyBase),
                 typeof(AlsoDerivedFromTestSagaWithTableNameAttributeActualSaga), typeof(AlsoDerivedFromTestSagaWithTableNameAttribute),
                 typeof(DerivedFromTestSagaWithTableNameAttributeActualSaga), typeof(DerivedFromTestSagaWithTableNameAttribute),
                 typeof(TestSagaWithTableNameAttributeActualSaga), typeof(TestSagaWithTableNameAttribute),
@@ -43,11 +39,11 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
                 typeof(SagaWithoutVersionedPropertyAttributeActualSaga), typeof(SagaWithoutVersionedPropertyAttribute),
                 typeof(object)
             };
-            metaModel.Initialize(types);
-            settings.Set(metaModel);
 
-            settings.Set("TypesToScan", types);
-            builder.ApplyMappings(settings, cfg);
+            metaModel.Initialize(types);
+
+            SagaModelMapper.AddMappings(cfg, metaModel, types);
+
             sessionFactory = cfg.BuildSessionFactory() as SessionFactoryImpl;
 
             persisterForTestSaga = sessionFactory.GetEntityPersisterFor<TestSagaData>();
@@ -56,7 +52,7 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
         [Test]
         public void Id_generator_should_be_set_to_assigned()
         {
-            Assert.AreEqual(persisterForTestSaga.IdentifierGenerator.GetType(), typeof (Assigned));
+            Assert.AreEqual(persisterForTestSaga.IdentifierGenerator.GetType(), typeof(Assigned));
         }
 
         [Test]
@@ -69,7 +65,7 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
         public void Related_entities_should_also_be_mapped()
         {
             Assert.AreEqual(sessionFactory.GetEntityPersisterFor<OrderLine>()
-                                .IdentifierGenerator.GetType(), typeof (GuidCombGenerator));
+                                .IdentifierGenerator.GetType(), typeof(GuidCombGenerator));
         }
 
         [Test]
@@ -88,10 +84,10 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
         public void Users_can_override_autoMappings_by_embedding_hbm_files()
         {
             Assert.AreEqual(sessionFactory.GetEntityPersisterFor<TestSagaWithHbmlXmlOverride>()
-                                .IdentifierGenerator.GetType(), typeof (IdentityGenerator));
+                                .IdentifierGenerator.GetType(), typeof(IdentityGenerator));
         }
 
-        [Test,Ignore("Not supported any more")]
+        [Test, Ignore("Not supported any more")]
         public void Inherited_property_classes_should_be_mapped()
         {
             persisterForTestSaga.ShouldContainMappingsFor<PolymorphicPropertyBase>();
@@ -102,7 +98,7 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
         public void Users_can_override_tableNames_by_using_an_attribute()
         {
             var persister =
-                sessionFactory.GetEntityPersister(typeof (TestSagaWithTableNameAttribute).FullName).ClassMetadata as
+                sessionFactory.GetEntityPersister(typeof(TestSagaWithTableNameAttribute).FullName).ClassMetadata as
                 AbstractEntityPersister;
             Assert.AreEqual(persister.RootTableName, "MyTestSchema.MyTestTable");
         }
@@ -111,7 +107,7 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
         public void Users_can_override_tableNames_by_using_an_attribute_which_does_not_derive()
         {
             var persister =
-                sessionFactory.GetEntityPersister(typeof (DerivedFromTestSagaWithTableNameAttribute).FullName).
+                sessionFactory.GetEntityPersister(typeof(DerivedFromTestSagaWithTableNameAttribute).FullName).
                     ClassMetadata as AbstractEntityPersister;
             Assert.AreEqual(persister.TableName, "DerivedFromTestSagaWithTableNameAttribute");
         }
@@ -120,7 +116,7 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
         public void Users_can_override_derived_tableNames_by_using_an_attribute()
         {
             var persister =
-                sessionFactory.GetEntityPersister(typeof (AlsoDerivedFromTestSagaWithTableNameAttribute).FullName).
+                sessionFactory.GetEntityPersister(typeof(AlsoDerivedFromTestSagaWithTableNameAttribute).FullName).
                     ClassMetadata as AbstractEntityPersister;
             Assert.AreEqual(persister.TableName, "MyDerivedTestTable");
         }
@@ -155,16 +151,16 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
         [Test]
         public void Versioned_Property_should_override_optimistic_lock()
         {
-          var persister1 = sessionFactory.GetEntityPersisterFor<SagaWithVersionedPropertyAttribute>();
-          var persister2 = sessionFactory.GetEntityPersisterFor<SagaWithoutVersionedPropertyAttribute>();
+            var persister1 = sessionFactory.GetEntityPersisterFor<SagaWithVersionedPropertyAttribute>();
+            var persister2 = sessionFactory.GetEntityPersisterFor<SagaWithoutVersionedPropertyAttribute>();
 
-          Assert.True(persister1.IsVersioned);
-          Assert.False(persister1.EntityMetamodel.IsDynamicUpdate);
-          Assert.AreEqual(Versioning.OptimisticLock.Version, persister1.EntityMetamodel.OptimisticLockMode);
+            Assert.True(persister1.IsVersioned);
+            Assert.False(persister1.EntityMetamodel.IsDynamicUpdate);
+            Assert.AreEqual(Versioning.OptimisticLock.Version, persister1.EntityMetamodel.OptimisticLockMode);
 
-          Assert.True(persister2.EntityMetamodel.IsDynamicUpdate);
-          Assert.AreEqual(Versioning.OptimisticLock.All, persister2.EntityMetamodel.OptimisticLockMode);
-          Assert.False(persister2.IsVersioned);
+            Assert.True(persister2.EntityMetamodel.IsDynamicUpdate);
+            Assert.AreEqual(Versioning.OptimisticLock.All, persister2.EntityMetamodel.OptimisticLockMode);
+            Assert.False(persister2.IsVersioned);
         }
     }
 
@@ -172,7 +168,7 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
     {
         public static IEntityPersister GetEntityPersisterFor<T>(this SessionFactoryImpl sessionFactory)
         {
-            return sessionFactory.GetEntityPersister(typeof (T).FullName);
+            return sessionFactory.GetEntityPersister(typeof(T).FullName);
         }
 
         public static void ShouldContainPersisterFor<T>(this SessionFactoryImpl sessionFactory)
@@ -186,7 +182,7 @@ namespace NServiceBus.SagaPersisters.NHibernate.Tests
         public static void ShouldContainMappingsFor<T>(this IEntityPersister persister)
         {
             var result = persister.EntityMetamodel.Properties
-                .Any(x => x.Type.ReturnedClass == typeof (T));
+                .Any(x => x.Type.ReturnedClass == typeof(T));
 
             Assert.True(result);
         }
