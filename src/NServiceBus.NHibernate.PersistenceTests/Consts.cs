21dace7cb9c47a26a47cbe60e51d2281d6dcf2cb
Szymon Pobiega <szymon.pobiega@gmail.com>
2020-07-28 08:44:32 +0200

    Add outbox variants
    Add oracle tests for sagas

--- src/NServiceBus.NHibernate.PersistenceTests/Consts.cs
+++ src/NServiceBus.NHibernate.PersistenceTests/Consts.cs
@@ -1,14 +1,22 @@
 ﻿namespace NServiceBus.NHibernate.PersistenceTests
 {
+    using System;
+
     static class Consts
     {
         const string @default = @"Data Source=.\SQLEXPRESS;Initial Catalog=nservicebus;Integrated Security=True;";
 
-        public static string SqlConnectionString
+        public static string ConnectionString
         {
             get
             {
-                var env = System.Environment.GetEnvironmentVariable("SQLServerConnectionString");
+                var env = Environment.GetEnvironmentVariable("NH_ACC_TEST_CONNSTR");
+                if (!string.IsNullOrEmpty(env))
+                {
+                    return env;
+                }
+
+                env = Environment.GetEnvironmentVariable("SQLServerConnectionString");
                 if (!string.IsNullOrEmpty(env))
                 {
                     return env;
--- src/NServiceBus.NHibernate.PersistenceTests/PersistenceTestsConfiguration.cs
+++ src/NServiceBus.NHibernate.PersistenceTests/PersistenceTestsConfiguration.cs
@@ -5,7 +5,9 @@
     using System.Reflection;
     using System.Threading.Tasks;
     using global::NHibernate.Cfg;
+    using global::NHibernate.Cfg.Loquacious;
     using global::NHibernate.Dialect;
+    using global::NHibernate.Driver;
     using global::NHibernate.Mapping.ByCode;
     using global::NHibernate.Tool.hbm2ddl;
     using NHibernate.Outbox;
@@ -13,13 +15,42 @@
     using NServiceBus.Outbox;
     using NServiceBus.Outbox.NHibernate;
     using NServiceBus.Sagas;
+    using NUnit.Framework;
     using Persistence;
     using Persistence.NHibernate;
     using SagaPersisters.NHibernate;
     using SagaPersisters.NHibernate.AutoPersistence;
 
+    class NHibernateVariant
+    {
+        public NHibernateVariant(string description, Action<IDbIntegrationConfigurationProperties> configureDb, IOutboxPersisterFactory outboxPersisterFactory, bool pessimistic = false, bool transactionScope = false)
+        {
+            ConfigureDb = configureDb;
+            OutboxPersisterFactory = outboxPersisterFactory;
+            Description = description;
+            Pessimistic = pessimistic;
+            TransactionScope = transactionScope;
+        }
+
+        public Action<IDbIntegrationConfigurationProperties> ConfigureDb { get; }
+        public IOutboxPersisterFactory OutboxPersisterFactory { get; }
+        public bool Pessimistic { get; }
+        public bool TransactionScope { get; }
+        public string Description { get; }
+
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+
     public partial class PersistenceTestsConfiguration
     {
+        public PersistenceTestsConfiguration()
+        {
+
+        }
+
         public bool SupportsDtc => false; // TODO: verify if this is true
         public bool SupportsOutbox => true;
         public bool SupportsFinders => true;  // TODO: verify if we actually need this as we think it should only be invoked by core
@@ -32,17 +63,64 @@
         public ISynchronizedStorageAdapter SynchronizedStorageAdapter { get; private set; }
         public IOutboxStorage OutboxStorage { get; private set; }
 
-        public async Task Configure()
+        static PersistenceTestsConfiguration()
         {
-            CurrentSessionHolder currentSessionHolder = null;
-            var outboxPersisterFactory = new OutboxPersisterFactory<OutboxRecord>();
-
-            var cfg = new Configuration()
-                .DataBaseIntegration(x =>
+            SagaVariants = new[]
+            {
+                CreateVariant("SQL Server", x =>
                 {
                     x.Dialect<MsSql2012Dialect>();
-                    x.ConnectionString = Consts.SqlConnectionString;
-                });
+                    x.ConnectionString = Consts.ConnectionString;
+                }),
+                CreateVariant("Oracle", x =>
+                {
+                    x.Dialect<Oracle10gDialect>();
+                    x.Driver<OracleManagedDataClientDriver>();
+                    x.ConnectionString = System.Environment.GetEnvironmentVariable("OracleConnectionString");
+                })
+            };
+            OutboxVariants = new[]
+            {
+                CreateVariant("SQL Server Optimistic Native default OutboxRecord", x =>
+                {
+                    x.Dialect<MsSql2012Dialect>();
+                    x.ConnectionString = Consts.ConnectionString;
+                }),
+                CreateVariant("SQL Server Pessimistic Native default OutboxRecord", x =>
+                {
+                    x.Dialect<MsSql2012Dialect>();
+                    x.ConnectionString = Consts.ConnectionString;
+                }, true, false),
+                CreateVariant("SQL Server Pessimistic TransactionScope default OutboxRecord", x =>
+                {
+                    x.Dialect<MsSql2012Dialect>();
+                    x.ConnectionString = Consts.ConnectionString;
+                }, true, true),
+                CreateVariant("SQL Server Optimistic TransactionScope default OutboxRecord", x =>
+                {
+                    x.Dialect<MsSql2012Dialect>();
+                    x.ConnectionString = Consts.ConnectionString;
+                }, false, true),
+            };
+        }
+
+        static TestFixtureData CreateVariant<T>(string description, Action<IDbIntegrationConfigurationProperties> configureDb, bool pessimistic = false, bool transactionScope = false) 
+            where T : class, IOutboxRecord, new()
+        {
+            return new TestFixtureData(new TestVariant(new NHibernateVariant(description, configureDb, new OutboxPersisterFactory<T>(), pessimistic, transactionScope)));
+        }
+
+        static TestFixtureData CreateVariant(string description, Action<IDbIntegrationConfigurationProperties> configureDb, bool pessimistic = false, bool transactionScope = false)
+        {
+            return CreateVariant<OutboxRecord>(description, configureDb, pessimistic, transactionScope);
+        }
+
+        public async Task Configure()
+        {
+            var variant = (NHibernateVariant) Variant.Values[0];
+
+            var cfg = new Configuration()
+                .DataBaseIntegration(variant.ConfigureDb);
 
             //Add mapping for the outbox record
             var mapper = new ModelMapper();
@@ -61,8 +139,6 @@
                 return type.DeclaringType.Name + "_" + type.Name;
             });
 
-            
-
             //Mark all map classes as not lazy because they don't declare their properties virtual
             foreach (var mapping in cfg.ClassMappings)
             {
@@ -76,57 +152,14 @@
 
             SagaIdGenerator = new DefaultSagaIdGenerator();
             SagaStorage = new SagaPersister();
-            SynchronizedStorage = new NHibernateSynchronizedStorage(sessionFactory, currentSessionHolder);
-            SynchronizedStorageAdapter = new NHibernateSynchronizedStorageAdapter(sessionFactory, currentSessionHolder);
-            OutboxStorage = outboxPersisterFactory.Create(sessionFactory, "TestEndpoint", false, false);
+            SynchronizedStorage = new NHibernateSynchronizedStorage(sessionFactory, null);
+            SynchronizedStorageAdapter = new NHibernateSynchronizedStorageAdapter(sessionFactory, null);
+            OutboxStorage = variant.OutboxPersisterFactory.Create(sessionFactory, "TestEndpoint", variant.Pessimistic, variant.TransactionScope);
         }
 
         public Task Cleanup()
         {
             return Task.CompletedTask;
-        }
-
-        class DefaultSagaIdGenerator : ISagaIdGenerator
-        {
-            public Guid Generate(SagaIdGeneratorContext context)
-            {
-                // intentionally ignore the property name and the value.
-                return CombGuid.Generate();
-            }
-        }
-
-        static class CombGuid
-        {
-            /// <summary>
-            /// Generate a new <see cref="Guid" /> using the comb algorithm.
-            /// </summary>
-            public static Guid Generate()
-            {
-                var guidArray = Guid.NewGuid().ToByteArray();
-
-                var now = DateTime.UtcNow;
-
-                // Get the days and milliseconds which will be used to build the byte string
-                var days = new TimeSpan(now.Ticks - BaseDateTicks);
-                var timeOfDay = now.TimeOfDay;
-
-                // Convert to a byte array
-                // Note that SQL Server is accurate to 1/300th of a millisecond so we divide by 3.333333
-                var daysArray = BitConverter.GetBytes(days.Days);
-                var millisecondArray = BitConverter.GetBytes((long)(timeOfDay.TotalMilliseconds / 3.333333));
-
-                // Reverse the bytes to match SQL Servers ordering
-                Array.Reverse(daysArray);
-                Array.Reverse(millisecondArray);
-
-                // Copy the bytes into the guid
-                Array.Copy(daysArray, daysArray.Length - 2, guidArray, guidArray.Length - 6, 2);
-                Array.Copy(millisecondArray, millisecondArray.Length - 4, guidArray, guidArray.Length - 4, 4);
-
-                return new Guid(guidArray);
-            }
-
-            static readonly long BaseDateTicks = new DateTime(1900, 1, 1).Ticks;
         }
     }
 }
