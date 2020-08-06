cccd75330aba1e9afd8b75b38f4876f817cf1aaa
Szymon Pobiega <szymon.pobiega@gmail.com>
2020-08-06 13:47:08 +0200

    Persistence tests (#457)
    * Added persistence tests project.
    
    * Make the outbox tests pass
    
    * Update persistence test sources
    
    * Add outbox variants
    
    Add oracle tests for sagas
    
    * Add Oracle driver
    
    * Shorten saga names
    
    * Limit the scope of mapping generation to the test class
    
    * Remove the timeout test
    
    * Disable oracle tests for linux core
    
    * Exclude complex types test
    
    * Ignore the test from the suite and try running another one with Task.run syntax
    
    * remove core dependency
    
    * update tests package
    
    * remove experimental test
    
    * update tests configuration
    
    * approve updated InternalsVisibleTo
    
    * remove unnecessary NuGet dependencies
    
    * revert FodyWeavers changes
    
    * revert DotSettings changes
    
    * Revert test project change
    
    * Align test adapter versions
    
    * Formatting
    
    * update persister test package
    
    Co-authored-by: lailabougria <laila.bougria@particular.net>
    Co-authored-by: Tim Bussmann <timbussmann@users.noreply.github.com>
    Co-authored-by: Brandon Ording <bording@gmail.com>

---
+++ src/NServiceBus.NHibernate.PersistenceTests/Consts.cs
@@ -0,0 +1,29 @@
+﻿namespace NServiceBus.NHibernate.PersistenceTests
+{
+    using System;
+
+    static class Consts
+    {
+        const string @default = @"Data Source=.\SQLEXPRESS;Initial Catalog=nservicebus;Integrated Security=True;";
+
+        public static string ConnectionString
+        {
+            get
+            {
+                var env = Environment.GetEnvironmentVariable("NH_ACC_TEST_CONNSTR");
+                if (!string.IsNullOrEmpty(env))
+                {
+                    return env;
+                }
+
+                env = Environment.GetEnvironmentVariable("SQLServerConnectionString");
+                if (!string.IsNullOrEmpty(env))
+                {
+                    return env;
+                }
+
+                return @default;
+            }
+        }
+    }
+}
---
+++ src/NServiceBus.NHibernate.PersistenceTests/PersistenceTestsConfiguration.cs
@@ -0,0 +1,197 @@
+﻿namespace NServiceBus.PersistenceTesting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using global::NHibernate.Cfg;
+    using global::NHibernate.Cfg.Loquacious;
+    using global::NHibernate.Dialect;
+    using global::NHibernate.Driver;
+    using global::NHibernate.Mapping.ByCode;
+    using global::NHibernate.Tool.hbm2ddl;
+    using NHibernate.Outbox;
+    using NHibernate.PersistenceTests;
+    using NServiceBus.Outbox;
+    using NServiceBus.Outbox.NHibernate;
+    using NServiceBus.Sagas;
+    using NUnit.Framework;
+    using Persistence;
+    using Persistence.NHibernate;
+    using SagaPersisters.NHibernate;
+    using SagaPersisters.NHibernate.AutoPersistence;
+
+    class NHibernateVariant
+    {
+        public NHibernateVariant(string description, Action<IDbIntegrationConfigurationProperties> configureDb, IOutboxPersisterFactory outboxPersisterFactory, bool pessimistic = false, bool transactionScope = false)
+        {
+            ConfigureDb = configureDb;
+            OutboxPersisterFactory = outboxPersisterFactory;
+            Description = description;
+            Pessimistic = pessimistic;
+            TransactionScope = transactionScope;
+        }
+
+        public Action<IDbIntegrationConfigurationProperties> ConfigureDb { get; }
+
+        public IOutboxPersisterFactory OutboxPersisterFactory { get; }
+
+        public bool Pessimistic { get; }
+
+        public bool TransactionScope { get; }
+
+        public string Description { get; }
+
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+
+    public partial class PersistenceTestsConfiguration
+    {
+        public bool SupportsDtc => false; // DTC tests are currently disabled due to CurrentSessionBehavior logic that is required to make this work
+
+        public bool SupportsOutbox => true;
+
+        public bool SupportsFinders => true;
+
+        public bool SupportsPessimisticConcurrency => true;
+
+        public ISagaIdGenerator SagaIdGenerator { get; private set; }
+
+        public ISagaPersister SagaStorage { get; private set; }
+
+        public ISynchronizedStorage SynchronizedStorage { get; private set; }
+
+        public ISynchronizedStorageAdapter SynchronizedStorageAdapter { get; private set; }
+
+        public IOutboxStorage OutboxStorage { get; private set; }
+
+        static PersistenceTestsConfiguration()
+        {
+            var sagaVariants = new List<object>
+            {
+                CreateVariant("SQL Server", x =>
+                {
+                    x.Dialect<MsSql2012Dialect>();
+                    x.ConnectionString = Consts.ConnectionString;
+                })
+            };
+
+            if (!string.IsNullOrWhiteSpace(System.Environment.GetEnvironmentVariable("OracleConnectionString")))
+            {
+                sagaVariants.Add(CreateVariant("Oracle", x =>
+                {
+                    x.Dialect<Oracle10gDialect>();
+                    x.Driver<OracleManagedDataClientDriver>();
+                    x.ConnectionString = System.Environment.GetEnvironmentVariable("OracleConnectionString");
+                }));
+            }
+            SagaVariants = sagaVariants.ToArray();
+            OutboxVariants = new[]
+            {
+                CreateVariant("SQL Server Optimistic Native default OutboxRecord", x =>
+                {
+                    x.Dialect<MsSql2012Dialect>();
+                    x.ConnectionString = Consts.ConnectionString;
+                }),
+                CreateVariant("SQL Server Pessimistic Native default OutboxRecord", x =>
+                {
+                    x.Dialect<MsSql2012Dialect>();
+                    x.ConnectionString = Consts.ConnectionString;
+                }, true, false),
+                CreateVariant("SQL Server Pessimistic TransactionScope default OutboxRecord", x =>
+                {
+                    x.Dialect<MsSql2012Dialect>();
+                    x.ConnectionString = Consts.ConnectionString;
+                }, true, true),
+                CreateVariant("SQL Server Optimistic TransactionScope default OutboxRecord", x =>
+                {
+                    x.Dialect<MsSql2012Dialect>();
+                    x.ConnectionString = Consts.ConnectionString;
+                }, false, true),
+            };
+        }
+
+        static TestFixtureData CreateVariant<T>(string description, Action<IDbIntegrationConfigurationProperties> configureDb, bool pessimistic = false, bool transactionScope = false)
+            where T : class, IOutboxRecord, new()
+        {
+            return new TestFixtureData(new TestVariant(new NHibernateVariant(description, configureDb, new OutboxPersisterFactory<T>(), pessimistic, transactionScope)));
+        }
+
+        static TestFixtureData CreateVariant(string description, Action<IDbIntegrationConfigurationProperties> configureDb, bool pessimistic = false, bool transactionScope = false)
+        {
+            return CreateVariant<OutboxRecord>(description, configureDb, pessimistic, transactionScope);
+        }
+
+        static bool BelongsToCurrentTest(Type t)
+        {
+            return t.DeclaringType != null && t.DeclaringType.FullName == TestContext.CurrentContext.Test.ClassName;
+        }
+
+        public async Task Configure()
+        {
+            var variant = (NHibernateVariant) Variant.Values[0];
+
+            var cfg = new Configuration()
+                .DataBaseIntegration(variant.ConfigureDb);
+
+            //Add mapping for the outbox record
+            var mapper = new ModelMapper();
+            mapper.AddMapping(typeof(OutboxRecordMapping));
+            cfg.AddMapping(mapper.CompileMappingForAllExplicitlyAddedEntities());
+
+            var allTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t => BelongsToCurrentTest(t)).ToList();
+            allTypes.Add(typeof(ContainSagaData));
+
+            var sagaTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t =>
+            {
+                return BelongsToCurrentTest(t)
+                       && (typeof(Saga).IsAssignableFrom(t)
+                           || typeof(IFindSagas<>).IsAssignableFrom(t)
+                           || typeof(IFinder).IsAssignableFrom(t));
+
+            }).ToArray();
+
+            sagaMetadataCollection = new SagaMetadataCollection();
+            sagaMetadataCollection.Initialize(sagaTypes);
+
+            SagaModelMapper.AddMappings(cfg, SagaMetadataCollection, allTypes, type => ShortenSagaName(type.Name));
+
+            //Mark all map classes as not lazy because they don't declare their properties virtual
+            foreach (var mapping in cfg.ClassMappings)
+            {
+                mapping.IsLazy = false;
+            }
+
+            var schema = new SchemaExport(cfg);
+            await schema.DropAsync(false, true);
+            await schema.CreateAsync(false, true);
+
+            var sessionFactory = cfg.BuildSessionFactory();
+
+            SagaIdGenerator = new DefaultSagaIdGenerator();
+            SagaStorage = new SagaPersister();
+            SynchronizedStorage = new NHibernateSynchronizedStorage(sessionFactory, null);
+            SynchronizedStorageAdapter = new NHibernateSynchronizedStorageAdapter(sessionFactory, null);
+            OutboxStorage = variant.OutboxPersisterFactory.Create(sessionFactory, "TestEndpoint", variant.Pessimistic, variant.TransactionScope);
+        }
+
+        static string ShortenSagaName(string sagaName)
+        {
+            return sagaName
+                .Replace("AnotherSagaWithCorrelatedProperty", "ASWCP")
+                .Replace("SagaWithCorrelationProperty", "SWCP")
+                .Replace("SagaWithoutCorrelationProperty", "SWOCP")
+                .Replace("SagaWithComplexType", "SWCT")
+                .Replace("TestSaga", "TS");
+        }
+
+        public Task Cleanup()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
