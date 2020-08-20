f91583670d050c8c48919ce24185420d08af1a87
Szymon Pobiega <szymon.pobiega@gmail.com>
2020-08-20 11:51:20 +0200

    Fix transaction scope outbox to allow floating the transaction scope to user code (#466)
    * Fix transaction scope outbox to allow floating the transaction scope to user code
    
    * Fix usings
    
    * Add comments to indicate why sync
    
    * Acceptance test
    
    * More comment
    
    * Drop schema in teardown
    
    * Reintroduce the disposing of the outbox transaction in case it faulted during creation
    
    * Fix When_using_transaction_scope_outbox_and_wrapping_handlers because that condition could never have been met now that the tx scope is properly wrapping the pipeline the core TransactionScopeUnitOfWorkBehavior  would always throw
    
    * Remove config.UnitOfWork().WrapHandlersInATransactionScope() because that is not a condition we can ever reach due to the core throwing earlier
    
    * fixing inspections
    
    * better name method argument name
    
    * refactoring `Begin` on `OutboxTransaction`
    
    Co-authored-by: danielmarbach <daniel.marbach@openplace.net>
    Co-authored-by: Tomek Masternak <tomasz.masternak@particular.net>

--- src/NServiceBus.NHibernate/Outbox/INHibernateOutboxTransaction.cs
+++ src/NServiceBus.NHibernate/Outbox/INHibernateOutboxTransaction.cs
@@ -9,6 +9,8 @@
     {
         ISession Session { get; }
         void OnSaveChanges(Func<Task> callback);
+        // Prepare is deliberately kept sync to allow floating of TxScope where needed
+        void Prepare();
         Task Begin(string endpointQualifiedMessageId);
         Task Complete(string endpointQualifiedMessageId, OutboxMessage outboxMessage, ContextBag context);
         void BeginSynchronizedSession(ContextBag context);
