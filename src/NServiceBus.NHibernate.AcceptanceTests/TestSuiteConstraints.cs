c082e9ad97f6768b3de6a579fc1ea109c9554ca9
mauroservienti <mauro.servienti@gmail.com>
2020-08-31 17:02:31 +0200

    Make sure that default ATs still use message driven pub/sub

--- src/NServiceBus.NHibernate.AcceptanceTests/TestSuiteConstraints.cs
+++ src/NServiceBus.NHibernate.AcceptanceTests/TestSuiteConstraints.cs
@@ -6,7 +6,7 @@
     {
         public bool SupportsDtc => false;
         public bool SupportsCrossQueueTransactions => true;
-        public bool SupportsNativePubSub => true;
+        public bool SupportsNativePubSub => false;
         public bool SupportsNativeDeferral => false;
         public bool SupportsOutbox => true;
         public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointAcceptanceTestingTransport(SupportsNativePubSub, SupportsNativeDeferral);
