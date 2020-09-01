7cfa75274e7d9a3e94b7eb2ef31bdbf70c081406
mauroservienti <mauro.servienti@gmail.com>
2020-09-01 10:59:02 +0200

    Bring back Gateway feature skeleton

---
+++ src/NServiceBus.NHibernate/Deduplication/DeduplicationConfig.cs
@@ -0,0 +1,38 @@
+﻿namespace NServiceBus.Persistence.NHibernate
+{
+    using System;
+    using global::NHibernate.Cfg;
+
+    /// <summary>
+    /// Deduplication configuration extensions.
+    /// </summary>
+    public static class DeduplicationConfig
+    {
+        /// <summary>
+        /// Disables automatic schema update.
+        /// </summary>
+        /// <param name="persistenceConfiguration"></param>
+        [ObsoleteEx(
+            Message = "NHibernate gateway persistence is deprecated. Use the new NServiceBus.Gateway.Sql dedicated package.",
+            RemoveInVersion = "10.0.0",
+            TreatAsErrorFromVersion = "9.0.0")]
+        public static PersistenceExtensions<NHibernatePersistence> DisableGatewayDeduplicationSchemaUpdate(this PersistenceExtensions<NHibernatePersistence> persistenceConfiguration)
+        {
+            throw new NotImplementedException("NHibernate gateway persistence is deprecated. Use the new NServiceBus.Gateway.Sql dedicated package.");
+        }
+
+        /// <summary>
+        /// Configures Subscription Storage to use the <paramref name="configuration"/>.
+        /// </summary>
+        /// <param name="persistenceConfiguration"></param>
+        /// <param name="configuration">The <see cref="Configuration"/> object.</param>
+        [ObsoleteEx(
+            Message = "NHibernate gateway persistence is deprecated. Use the new NServiceBus.Gateway.Sql dedicated package.",
+            RemoveInVersion = "10.0.0",
+            TreatAsErrorFromVersion = "9.0.0")]
+        public static PersistenceExtensions<NHibernatePersistence> UseGatewayDeduplicationConfiguration(this PersistenceExtensions<NHibernatePersistence> persistenceConfiguration, Configuration configuration)
+        {
+            throw new NotImplementedException("NHibernate gateway persistence is deprecated. Use the new NServiceBus.Gateway.Sql dedicated package.");
+        }
+    }
+}
---
+++ src/NServiceBus.NHibernate/Deduplication/NHibernateGatewayDeduplication.cs
@@ -0,0 +1,20 @@
+﻿namespace NServiceBus.Features
+{
+    /// <summary>
+    /// NHibernate Gateway Deduplication.
+    /// </summary>
+    [ObsoleteEx(
+            Message = "NHibernate gateway persistence is deprecated. Use the new NServiceBus.Gateway.Sql dedicated package.",
+            RemoveInVersion = "10.0.0",
+            TreatAsErrorFromVersion = "9.0.0")]
+    public class NHibernateGatewayDeduplication : Feature
+    {
+        /// <summary>
+        /// Called when the feature should perform its initialization. This call will only happen if the feature is enabled.
+        /// </summary>
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+
+        }
+    }
+}
