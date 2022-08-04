d5c7f4267fd91f365a7ef9963cb717517e567236
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-08-04 13:02:53 +0200

    fix synchronized session dispose (#816)
    to not throw when session is disposed without successfully opening it.

---
+++ src/NServiceBus.NHibernate.Tests/SynchronizedStorage/NHibernateSynchronizedStorageSessionTests.cs
@@ -0,0 +1,18 @@
+﻿namespace NServiceBus.NHibernate.Tests.SynchronizedStorage
+{
+    using NHibernate.SynchronizedStorage;
+    using NUnit.Framework;
+    using Persistence.NHibernate;
+
+    [TestFixture]
+    public class NHibernateSynchronizedStorageSessionTests
+    {
+        [Test]
+        public void Should_not_throw_when_disposing_without_opened_session()
+        {
+            var synchronizedSession = new NHibernateSynchronizedStorageSession(new SessionFactoryHolder(null));
+
+            Assert.DoesNotThrow(() => synchronizedSession.Dispose());
+        }
+    }
+}
