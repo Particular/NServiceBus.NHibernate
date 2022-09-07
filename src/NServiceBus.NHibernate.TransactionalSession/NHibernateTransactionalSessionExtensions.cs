557d858b34a9b8babc7eea53eaebb95ecc20154e
Tomasz Masternak <tomasz.masternak@particular.net>
2022-09-07 12:26:53 +0200

    Implement TransactionalSession as part of the persistence (#829)
    * persister specific logic in the persister repo
    
    * api approvals
    
    * support paramterless Open session method
    
    * consistent namespace declaration (all in block-mode)
    
    * Align with other persistences
    
    * Extract constant
    
    * Adjust namespace
    
    * Update to the latest tx session updates
    
    * Take an explicit reference
    
    * Bump RC3
    
    * Base setup
    
    Co-authored-by: danielmarbach <daniel.marbach@openplace.net>
    # Conflicts:
    #	src/NServiceBus.NHibernate/NServiceBus.NHibernate.csproj

---
+++ src/NServiceBus.NHibernate.TransactionalSession/NHibernateTransactionalSessionExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace NServiceBus.TransactionalSession
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Configuration.AdvancedExtensibility;
+    using Features;
+
+    /// <summary>
+    /// Enables the transactional session feature.
+    /// </summary>
+    public static class NHibernateTransactionalSessionExtensions
+    {
+        /// <summary>
+        /// Enables transactional session for this endpoint.
+        /// </summary>
+        public static PersistenceExtensions<NHibernatePersistence> EnableTransactionalSession(
+            this PersistenceExtensions<NHibernatePersistence> persistenceExtensions)
+        {
+            persistenceExtensions.GetSettings().EnableFeatureByDefault(typeof(NHibernateTransactionalSession));
+
+            return persistenceExtensions;
+        }
+
+        /// <summary>
+        /// Opens the transactional session
+        /// </summary>
+        public static Task Open(this ITransactionalSession transactionalSession, CancellationToken cancellationToken = default)
+            => transactionalSession.Open(new NHibernateOpenSessionOptions(), cancellationToken);
+    }
+}
