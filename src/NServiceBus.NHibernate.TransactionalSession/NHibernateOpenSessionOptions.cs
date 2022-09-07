96dd3d758c6a5845bdb9e4f9d9639f85ec556d16
Tomasz Masternak <tomasz.masternak@particular.net>
2022-09-07 12:26:53 +0200

    Implement TransactionalSession as part of the persistence (#829)
    * persister specific logic in the persister repo
    
    * api approvals
    
    * support paramterless Open session method
    
    * consistent namespace declaration (all in block-mode)
    
    * Align with other persistences
    
    * Extract constant
    
    * Adjust namespace
    
    * Update to the latest tx session updates
    
    * Take an explicit reference
    
    * Bump RC3
    
    * Base setup
    
    Co-authored-by: danielmarbach <daniel.marbach@openplace.net>

---
+++ src/NServiceBus.NHibernate.TransactionalSession/NHibernateOpenSessionOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace NServiceBus.TransactionalSession
+{
+    /// <summary>
+    /// The options allowing to control the behavior of the transactional session.
+    /// </summary>
+    public class NHibernateOpenSessionOptions : OpenSessionOptions
+    {
+        internal void SetEndpointQualifiedMessageIdValue(string endpointName)
+        {
+            var endpointQualifiedMessageId = $"{endpointName}/{SessionId}";
+
+            Extensions.Set(EndpointQualifiedMessageIdKeyName, endpointQualifiedMessageId);
+            Metadata.Add(EndpointQualifiedMessageIdKeyName, endpointQualifiedMessageId);
+        }
+
+        const string EndpointQualifiedMessageIdKeyName = "NServiceBus.Persistence.NHibernate.EndpointQualifiedMessageId";
+    }
+}
