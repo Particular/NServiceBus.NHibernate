3c4f1d467d8665bf74d4a9a5ad3f59486742a568
danielmarbach <daniel.marbach@openplace.net>
2023-03-22 10:41:57 +0100

    Move saga data out

--- src/NServiceBus.NHibernate.PersistenceTests/When_multiple_sagas_in_outbox_transaction.cs
+++ src/NServiceBus.NHibernate.PersistenceTests/When_multiple_sagas_in_outbox_transaction.cs
@@ -11,8 +11,8 @@ namespace NServiceBus.PersistenceTesting.Sagas
         {
             configuration.RequiresOutboxSupport();
 
-            var saga1 = new Saga1.Saga1Data { CorrelationId = Guid.NewGuid().ToString() };
-            var saga2 = new Saga2.Saga2Data { CorrelationId = Guid.NewGuid().ToString() };
+            var saga1 = new Saga1Data { CorrelationId = Guid.NewGuid().ToString() };
+            var saga2 = new Saga2Data { CorrelationId = Guid.NewGuid().ToString() };
 
             var context = configuration.GetContextBagForOutbox();
             var outboxMessage = await configuration.OutboxStorage.Get(Guid.NewGuid().ToString(), context);
@@ -22,7 +22,7 @@ namespace NServiceBus.PersistenceTesting.Sagas
                 using (var saga1Session = configuration.CreateStorageSession())
                 {
                     await saga1Session.TryOpen(outboxTransaction, context);
-                    var saga1Data = await configuration.SagaStorage.Get<Saga1.Saga1Data>(nameof(Saga1.Saga1Data.CorrelationId),
+                    var saga1Data = await configuration.SagaStorage.Get<Saga1Data>(nameof(Saga1Data.CorrelationId),
                         saga1.CorrelationId, saga1Session, context);
                     Assert.IsNull(saga1Data);
 
@@ -34,7 +34,7 @@ namespace NServiceBus.PersistenceTesting.Sagas
                 using (var saga2Session = configuration.CreateStorageSession())
                 {
                     await saga2Session.TryOpen(outboxTransaction, context);
-                    var saga2Data = await configuration.SagaStorage.Get<Saga2.Saga2Data>(nameof(Saga2.Saga2Data.CorrelationId),
+                    var saga2Data = await configuration.SagaStorage.Get<Saga2Data>(nameof(Saga2Data.CorrelationId),
                         saga2.CorrelationId, saga2Session, context);
                     Assert.IsNull(saga2Data);
 
@@ -46,10 +46,10 @@ namespace NServiceBus.PersistenceTesting.Sagas
                 await outboxTransaction.Commit();
             }
 
-            var sagaData1 = await GetById<Saga1.Saga1Data>(saga1.Id);
+            var sagaData1 = await GetById<Saga1Data>(saga1.Id);
             Assert.NotNull(sagaData1);
             Assert.AreEqual(saga1.CorrelationId, sagaData1.CorrelationId);
-            var sagaData2 = await GetById<Saga2.Saga2Data>(saga2.Id);
+            var sagaData2 = await GetById<Saga2Data>(saga2.Id);
             Assert.NotNull(sagaData2);
             Assert.AreEqual(saga2.CorrelationId, sagaData2.CorrelationId);
         }
@@ -59,8 +59,8 @@ namespace NServiceBus.PersistenceTesting.Sagas
         {
             configuration.RequiresOutboxSupport();
 
-            var saga1 = new Saga1.Saga1Data { CorrelationId = Guid.NewGuid().ToString() };
-            var saga2 = new Saga2.Saga2Data { CorrelationId = Guid.NewGuid().ToString() };
+            var saga1 = new Saga1Data { CorrelationId = Guid.NewGuid().ToString() };
+            var saga2 = new Saga2Data { CorrelationId = Guid.NewGuid().ToString() };
 
             var context = configuration.GetContextBagForOutbox();
             var outboxMessage = await configuration.OutboxStorage.Get(Guid.NewGuid().ToString(), context);
@@ -70,7 +70,7 @@ namespace NServiceBus.PersistenceTesting.Sagas
                 using (var saga1Session = configuration.CreateStorageSession())
                 {
                     await saga1Session.TryOpen(outboxTransaction, context);
-                    var saga1Data = await configuration.SagaStorage.Get<Saga1.Saga1Data>(nameof(Saga1.Saga1Data.CorrelationId),
+                    var saga1Data = await configuration.SagaStorage.Get<Saga1Data>(nameof(Saga1Data.CorrelationId),
                         saga1.CorrelationId, saga1Session, context);
                     Assert.IsNull(saga1Data);
 
@@ -82,7 +82,7 @@ namespace NServiceBus.PersistenceTesting.Sagas
                 using (var saga2Session = configuration.CreateStorageSession())
                 {
                     await saga2Session.TryOpen(outboxTransaction, context);
-                    var saga2Data = await configuration.SagaStorage.Get<Saga2.Saga2Data>(nameof(Saga2.Saga2Data.CorrelationId),
+                    var saga2Data = await configuration.SagaStorage.Get<Saga2Data>(nameof(Saga2Data.CorrelationId),
                         saga2.CorrelationId, saga2Session, context);
                     Assert.IsNull(saga2Data);
 
@@ -94,9 +94,9 @@ namespace NServiceBus.PersistenceTesting.Sagas
                 // no commit
             }
 
-            var sagaData1 = await GetById<Saga1.Saga1Data>(saga1.Id);
+            var sagaData1 = await GetById<Saga1Data>(saga1.Id);
             Assert.IsNull(sagaData1);
-            var sagaData2 = await GetById<Saga2.Saga2Data>(saga2.Id);
+            var sagaData2 = await GetById<Saga2Data>(saga2.Id);
             Assert.IsNull(sagaData2);
         }
 
@@ -105,9 +105,9 @@ namespace NServiceBus.PersistenceTesting.Sagas
         {
             configuration.RequiresOutboxSupport();
 
-            var saga1 = new Saga1.Saga1Data { CorrelationId = Guid.NewGuid().ToString(), SomeSaga1Data = "saga 1 before update" };
+            var saga1 = new Saga1Data { CorrelationId = Guid.NewGuid().ToString(), SomeSaga1Data = "saga 1 before update" };
             await SaveSaga(saga1);
-            var saga2 = new Saga2.Saga2Data { CorrelationId = Guid.NewGuid().ToString(), SomeSaga2Data = "saga 2 before update" };
+            var saga2 = new Saga2Data { CorrelationId = Guid.NewGuid().ToString(), SomeSaga2Data = "saga 2 before update" };
             await SaveSaga(saga2);
 
             var context = configuration.GetContextBagForOutbox();
@@ -119,7 +119,7 @@ namespace NServiceBus.PersistenceTesting.Sagas
                 {
                     await saga1Session.TryOpen(outboxTransaction, context);
 
-                    var existingSaga1 = await configuration.SagaStorage.Get<Saga1.Saga1Data>(saga1.Id, saga1Session, context);
+                    var existingSaga1 = await configuration.SagaStorage.Get<Saga1Data>(saga1.Id, saga1Session, context);
                     existingSaga1.SomeSaga1Data = "saga 1 after update";
 
                     await configuration.SagaStorage.Update(existingSaga1, saga1Session, context);
@@ -131,7 +131,7 @@ namespace NServiceBus.PersistenceTesting.Sagas
                 {
                     await saga2Session.TryOpen(outboxTransaction, context);
 
-                    var existingSaga2 = await configuration.SagaStorage.Get<Saga2.Saga2Data>(saga2.Id, saga2Session, context);
+                    var existingSaga2 = await configuration.SagaStorage.Get<Saga2Data>(saga2.Id, saga2Session, context);
                     existingSaga2.SomeSaga2Data = "saga 2 after update";
 
                     await configuration.SagaStorage.Update(existingSaga2, saga2Session, context);
@@ -142,10 +142,10 @@ namespace NServiceBus.PersistenceTesting.Sagas
                 await outboxTransaction.Commit();
             }
 
-            var saga1AfterUpdate = await GetById<Saga1.Saga1Data>(saga1.Id);
+            var saga1AfterUpdate = await GetById<Saga1Data>(saga1.Id);
             Assert.NotNull(saga1AfterUpdate);
             Assert.AreEqual("saga 1 after update", saga1AfterUpdate.SomeSaga1Data);
-            var saga2AfterUpdate = await GetById<Saga2.Saga2Data>(saga2.Id);
+            var saga2AfterUpdate = await GetById<Saga2Data>(saga2.Id);
             Assert.NotNull(saga2AfterUpdate);
             Assert.AreEqual("saga 2 after update", saga2AfterUpdate.SomeSaga2Data);
         }
@@ -155,9 +155,9 @@ namespace NServiceBus.PersistenceTesting.Sagas
         {
             configuration.RequiresOutboxSupport();
 
-            var saga1 = new Saga1.Saga1Data { CorrelationId = Guid.NewGuid().ToString(), SomeSaga1Data = "saga 1 before update" };
+            var saga1 = new Saga1Data { CorrelationId = Guid.NewGuid().ToString(), SomeSaga1Data = "saga 1 before update" };
             await SaveSaga(saga1);
-            var saga2 = new Saga2.Saga2Data { CorrelationId = Guid.NewGuid().ToString(), SomeSaga2Data = "saga 2 before update" };
+            var saga2 = new Saga2Data { CorrelationId = Guid.NewGuid().ToString(), SomeSaga2Data = "saga 2 before update" };
             await SaveSaga(saga2);
 
             var context = configuration.GetContextBagForOutbox();
@@ -169,7 +169,7 @@ namespace NServiceBus.PersistenceTesting.Sagas
                 {
                     await saga1Session.TryOpen(outboxTransaction, context);
 
-                    var existingSaga1 = await configuration.SagaStorage.Get<Saga1.Saga1Data>(saga1.Id, saga1Session, context);
+                    var existingSaga1 = await configuration.SagaStorage.Get<Saga1Data>(saga1.Id, saga1Session, context);
                     existingSaga1.SomeSaga1Data = "saga 1 after update";
 
                     await configuration.SagaStorage.Update(existingSaga1, saga1Session, context);
@@ -181,7 +181,7 @@ namespace NServiceBus.PersistenceTesting.Sagas
                 {
                     await saga2Session.TryOpen(outboxTransaction, context);
 
-                    var existingSaga2 = await configuration.SagaStorage.Get<Saga2.Saga2Data>(saga2.Id, saga2Session, context);
+                    var existingSaga2 = await configuration.SagaStorage.Get<Saga2Data>(saga2.Id, saga2Session, context);
                     existingSaga2.SomeSaga2Data = "saga 2 after update";
 
                     await configuration.SagaStorage.Update(existingSaga2, saga2Session, context);
@@ -192,38 +192,38 @@ namespace NServiceBus.PersistenceTesting.Sagas
                 // no commit
             }
 
-            var saga1AfterUpdate = await GetById<Saga1.Saga1Data>(saga1.Id);
+            var saga1AfterUpdate = await GetById<Saga1Data>(saga1.Id);
             Assert.NotNull(saga1AfterUpdate);
             Assert.AreEqual("saga 1 before update", saga1AfterUpdate.SomeSaga1Data);
-            var saga2AfterUpdate = await GetById<Saga2.Saga2Data>(saga2.Id);
+            var saga2AfterUpdate = await GetById<Saga2Data>(saga2.Id);
             Assert.NotNull(saga2AfterUpdate);
             Assert.AreEqual("saga 2 before update", saga2AfterUpdate.SomeSaga2Data);
         }
 
-        public class Saga1 : Saga<Saga1.Saga1Data>, IAmStartedByMessages<StartTestSagaMessage>
+        public class Saga1 : Saga<Saga1Data>, IAmStartedByMessages<StartTestSagaMessage>
         {
             protected override void ConfigureHowToFindSaga(SagaPropertyMapper<Saga1Data> mapper) => mapper.ConfigureMapping<StartTestSagaMessage>(m => m.CorrelationProperty).ToSaga(s => s.CorrelationId);
 
             public Task Handle(StartTestSagaMessage message, IMessageHandlerContext context) => throw new NotImplementedException();
-
-            public class Saga1Data : ContainSagaData
-            {
-                public string CorrelationId { get; set; }
-                public string SomeSaga1Data { get; set; }
-            }
         }
 
-        public class Saga2 : Saga<Saga2.Saga2Data>, IAmStartedByMessages<StartTestSagaMessage>
+        public class Saga1Data : ContainSagaData
+        {
+            public string CorrelationId { get; set; }
+            public string SomeSaga1Data { get; set; }
+        }
+
+        public class Saga2 : Saga<Saga2Data>, IAmStartedByMessages<StartTestSagaMessage>
         {
             protected override void ConfigureHowToFindSaga(SagaPropertyMapper<Saga2Data> mapper) => mapper.ConfigureMapping<StartTestSagaMessage>(m => m.CorrelationProperty).ToSaga(s => s.CorrelationId);
 
             public Task Handle(StartTestSagaMessage message, IMessageHandlerContext context) => throw new NotImplementedException();
+        }
 
-            public class Saga2Data : ContainSagaData
-            {
-                public string CorrelationId { get; set; }
-                public string SomeSaga2Data { get; set; }
-            }
+        public class Saga2Data : ContainSagaData
+        {
+            public string CorrelationId { get; set; }
+            public string SomeSaga2Data { get; set; }
         }
 
         public class StartTestSagaMessage : IMessage
