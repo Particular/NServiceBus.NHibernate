456f355750e13f0ed11c75746b855d0203743aa1
Tamara Rivera <tamita.rivera@gmail.com>
2024-03-12 15:17:29 -0700

    Undoing the change of the version of APIApprovals.

--- src/NServiceBus.NHibernate.Tests/API/APIApprovals.cs
+++ src/NServiceBus.NHibernate.Tests/API/APIApprovals.cs
@@ -10,10 +10,12 @@
         [Test]
         public void Approve()
         {
-            var publicApi = typeof(NHibernatePersistence).Assembly.GeneratePublicApi(new ApiGeneratorOptions
-            {
-                ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" }
-            });
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(NHibernatePersistence).Assembly,
+                excludeAttributes: new[]
+                {
+                    "System.Runtime.Versioning.TargetFrameworkAttribute",
+                    "System.Reflection.AssemblyMetadataAttribute"
+                });
             Approver.Verify(publicApi);
         }
     }
