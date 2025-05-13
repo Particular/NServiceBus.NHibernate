7ea1c77a5497a2de7efaa238ac275d38e3822783
Christian <christian.quiros+github@gmamil.com>
2025-05-13 09:44:11 -0700

    Add new API to control endpoint name

--- src/NServiceBus.NHibernate/Outbox/NHibernateOutbox.cs
+++ src/NServiceBus.NHibernate/Outbox/NHibernateOutbox.cs
@@ -24,7 +24,7 @@ namespace NServiceBus.Features
         internal const string OutboxTransactionModeSettingsKey = "NServiceBus.NHibernate.OutboxTransactionScopeMode";
         internal const string OutboxTransactionIsolationLevelSettingsKey = "NServiceBus.NHibernate.OutboxTransactionIsolationLevel";
         internal const string OutboxTransactionScopeModeIsolationLevelSettingsKey = "NServiceBus.NHibernate.OutboxTransactionScopeModeIsolationLevel";
-
+        internal const string EndpointNameKey = "NHibernate.Outbox.EndpointName";
         /// <summary>
         /// Creates a new instance of the feature
         /// </summary>
@@ -73,7 +73,8 @@ namespace NServiceBus.Features
             context.Services.AddSingleton<IOutboxStorage>(sp =>
             {
                 var holder = sp.GetRequiredService<SessionFactoryHolder>();
-                var persister = persisterFactory.Create(holder.SessionFactory, context.Settings.EndpointName(), pessimisticMode, transactionScopeMode, adoIsolationLevel, transactionScopeIsolationLevel);
+                var endpointName = context.Settings.GetOrDefault<string>(EndpointNameKey) ?? context.Settings.EndpointName();
+                var persister = persisterFactory.Create(holder.SessionFactory, endpointName, pessimisticMode, transactionScopeMode, adoIsolationLevel, transactionScopeIsolationLevel);                
                 return persister;
             });
 
